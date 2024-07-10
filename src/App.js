3fbf6e14bd63f0b341f1efb2ea5b9f36ef3f4e4c
ulugbek <vulugb@gmail.com>
2024-07-10 22:18:17 +0500

    add jsconfig file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
-import "./App.css";
-import { useSelectors } from "./hooks/useSelectors";
-import { Button } from "./ui/Button/Button";
+import "App.css";
+import { useSelectors } from "hooks/useSelectors";
+import { Button } from "ui/Button/Button";
 
 function App() {
 	const { test } = useSelectors();
